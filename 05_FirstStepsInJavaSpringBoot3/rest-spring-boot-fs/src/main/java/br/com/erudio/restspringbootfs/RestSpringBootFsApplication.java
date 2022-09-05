6225d43abb90d0362d04883fcdf7704b734fbd80
Ian Alves <ianh.alves@gmail.com>
2022-09-05 17:56:53 -0300

    First Steps in Java With Spring Boot 3

---
+++ 05_FirstStepsInJavaSpringBoot3/rest-spring-boot-fs/src/main/java/br/com/erudio/restspringbootfs/RestSpringBootFsApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restspringbootfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootFsApplication.class, args);
+	}
+
+}
